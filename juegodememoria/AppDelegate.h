05279f2c293af4c1f9babd2ee91891e56f6c43ad
Leopoldo Marquez <leopoldomarquez@leopoldos-mac-mini.local>
2015-12-12 13:14:29 -0800

    Initial Commit

---
+++ juegodememoria/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  juegodememoria
+//
+//  Created by Leopoldo Marquez on 12/12/15.
+//  Copyright © 2015 Leopoldo Marquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ juegodememoria/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  juegodememoria
+//
+//  Created by Leopoldo Marquez on 12/12/15.
+//  Copyright © 2015 Leopoldo Marquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
